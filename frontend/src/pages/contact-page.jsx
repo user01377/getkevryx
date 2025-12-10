2ae00ba305c2c45715dd215da166386d557c7fbd
Vincent Vue <vuevincent2025@gmail.com>
2025-12-10 12:38:11 -0500

    created basic contact page with styling

--- frontend/src/pages/contact-page.jsx
+++ frontend/src/pages/contact-page.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import "../styles/contact-page.css";
 
 export default function Contact() {
-    return (
-        <div className="landing-page">
-            <h1>kevryx.</h1>
-        </div>
-    );
+  return (
+    <main className="contact-page page">
+        <h1>Contact The Team</h1>
+
+        <form className="contact-form">
+            <label htmlFor="name">Full Name</label>
+            <input type="text" id="name" name="name" placeholder="Your Name" required />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="your@email.com" required />
+
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" placeholder="Write your message..." rows="5" required></textarea>
+
+            <button type="submit">Send</button>
+        </form>
+    </main>
+  );
 }
