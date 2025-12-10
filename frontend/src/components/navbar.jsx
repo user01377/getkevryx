a57f203f9aaaf05f9bb607d7d0de4cb368a02a90
Vincent Vue <vuevincent2025@gmail.com>
2025-12-10 08:47:51 -0500

    restructued navbar into 3 divs and added appropriate styling

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,21 +3,28 @@ import "../styles/navbar.css";
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            
-            
-
-            <nav className="nav-links">
-                <a href="/" className="nav-products">
+        <nav className="navbar">
+            {/* split the navbar into 3 sections so styling is easier */}
+            <div className="nav-left">
+                <a href="/" className="nav-logo">
                     <img src="/logo.svg" alt="Logo" className="navbar-logo" />
+                    <span className="logo-text">Kevryx</span>
                 </a>
+            </div>
+
+            <div className="nav-center">
                 <a href="/" className="nav-products">Products</a>
                 <a href="/" className="nav-faq">FAQ</a>
                 <a href="/" className="nav-mission">Mission</a>
+            </div>
+
+            <div className="nav-right">
                 <a href="/" className="nav-cart">
                     <img src="/shopping-cart.svg" alt="Cart" />
                 </a>
-            </nav>
-        </div>
+            </div>
+
+        </nav>
+
     );
 }
