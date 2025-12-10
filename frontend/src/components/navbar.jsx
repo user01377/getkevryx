a3f2ba66905900bdf940bda481f5a747a263cbc3
Vincent Vue <vuevincent2025@gmail.com>
2025-12-10 12:04:41 -0500

    replaced a tags with Link tags provided by react router

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,31 +1,29 @@
 import React from "react";
 import "../styles/navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            {/* split the navbar into 3 sections so styling is easier */}
             <div className="nav-left">
-                <a href="/" className="nav-logo">
-                    <img src="/logo.svg" alt="Logo" className="navbar-logo" />
-                    <span className="logo-text">Kevryx</span>
-                </a>
+                <Link to="/" className="nav-logo">
+                <img src="/logo.svg" alt="Logo" className="navbar-logo" />
+                <span className="logo-text">Kevryx</span>
+                </Link>
             </div>
 
             <div className="nav-center">
-                <a href="/" className="nav-products">Products</a>
-                <a href="/" className="nav-mission">Mission</a>
-                <a href="/" className="nav-faq">FAQ</a>
-                <a href="/contact" className="nav-contact">Contact</a>
+                <Link to="/" className="nav-products">Products</Link>
+                <Link to="/" className="nav-mission">Mission</Link>
+                <Link to="/" className="nav-faq">FAQ</Link>
+                <Link to="/contact" className="nav-contact">Contact</Link>
             </div>
 
             <div className="nav-right">
-                <a href="/" className="nav-cart">
-                    <img src="/shopping-cart.svg" alt="Cart" />
-                </a>
+                <Link to="/" className="nav-cart">
+                <img src="/shopping-cart.svg" alt="Cart" />
+                </Link>
             </div>
-
         </nav>
-
     );
 }
