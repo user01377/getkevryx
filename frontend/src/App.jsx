75578b0e4d55255df591ec5837abe286f677dc8e
Vincent Vue <vuevincent2025@gmail.com>
2025-12-12 12:52:46 -0500

    removed .css file for banners and put the css styling inside of app.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css"
-import "./styles/hero-image.css"
 
 import LandingPage from "./pages/landing-page";
 import Contact from "./pages/contact-page";
