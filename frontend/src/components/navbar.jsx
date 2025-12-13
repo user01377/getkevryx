fc38755c7dcd3e0e3e35d3dd597038a9511b6b61
Vincent Vue <vuevincent2025@gmail.com>
2025-12-13 00:32:12 -0500

    modernized styling of navbar with underline animation, interactive icons, and smaller text

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,33 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const [hidden, setHidden] = useState(false);
-
-  useEffect(() => {
-    let lastScrollY = window.scrollY;
-    const scrollThreshold = 10; // higher means more pixels needed to hide/show navbar
-  
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-  
-      if (currentScrollY > lastScrollY + scrollThreshold) {
-        setHidden(true);
-      } else if (currentScrollY < lastScrollY - scrollThreshold) {
-        setHidden(false);
-      }
-  
-      lastScrollY = currentScrollY;
-    };
-  
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-  
-
   return (
-    <nav className={`navbar ${hidden ? "hidden" : ""}`}>
+
+    <nav className="navbar">
       <div className="nav-left">
         <Link to="/" className="nav-logo">
           <img src="/logo.svg" alt="Logo" className="navbar-logo" />
@@ -36,10 +14,10 @@ export default function Navbar() {
       </div>
 
       <div className="nav-center">
-        <Link to="/" className="nav-products">ALL PRODUCTS</Link>
-        <Link to="/mission" className="nav-mission">MISSION</Link>
-        <Link to="/faq" className="nav-faq">FAQ</Link>
-        <Link to="/contact" className="nav-contact">CONTACT</Link>
+        <Link to="/" className="nav-link">ALL PRODUCTS</Link>
+        <Link to="/mission" className="nav-link">MISSION</Link>
+        <Link to="/faq" className="nav-link">FAQ</Link>
+        <Link to="/contact" className="nav-link">CONTACT</Link>
       </div>
 
       <div className="nav-right">
@@ -48,5 +26,6 @@ export default function Navbar() {
         </Link>
       </div>
     </nav>
+    
   );
 }
