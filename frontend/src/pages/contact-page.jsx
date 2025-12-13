00e5fd95cf1438e3329a21442b764f1effe18e56
Vincent Vue <vuevincent2025@gmail.com>
2025-12-12 23:44:51 -0500

    added more componnents to contact page to fill space and make page feel less empty
    added corresponding styling with new tags as well

--- frontend/src/pages/contact-page.jsx
+++ frontend/src/pages/contact-page.jsx
@@ -3,36 +3,44 @@ import "../styles/contact-page.css";
 
 export default function Contact() {
   return (
-    <main className="contact-page page">
-        <h1>Contact Us</h1>
-        <h1>Have A Question For Us?</h1>
-        <p>Our Customer Service Team is actively working on replying to inquiries. Currently, our estimated response time is 1-2 business days.</p>
-        <p>Please see the FAQ page before submitting a contact form. Thank You!</p>
+<main className="contact-page page">
 
-        <form className="contact-form">
+    <h1 className="contact-header">Contact Us</h1>
 
-          <label htmlFor="name">Subject</label>
-          <input type="text" id="subject" name="subject" placeholder="Please enter a subject" required />
+    <div className="contact-intro">
+        <h2 className="contact-subheader">Have A Question For Us?</h2>
+        <p className="contact-subtext">
+            Our Customer Service Team is actively working on replying to inquiries. 
+            Currently, our estimated response time is 1-2 business days.
+        </p>
+        <p className="contact-subtext">
+            Please see the FAQ page before submitting a contact form. Thank You!
+        </p>
+    </div>
 
-          <label htmlFor="name">First Name</label>
-          <input type="text" id="name" name="name" required />
+    <form className="contact-form">
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id="subject" name="subject" placeholder="Please enter a subject" required />
 
-          <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" name="name" required />
+        <label htmlFor="name">First Name</label>
+        <input type="text" id="name" name="name" required />
 
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="your@email.com" required />
+        <label htmlFor="lastname">Last Name</label>
+        <input type="text" id="lastname" name="lastname" required />
 
-          <label htmlFor="email">Phone Number</label>
-          <input type="text" id="phone" name="phone" placeholder="ex. 123-456-6789" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="your@email.com" required />
 
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" rows="5" required></textarea>
+        <label htmlFor="phone">Phone Number</label>
+        <input type="text" id="phone" name="phone" placeholder="ex. 123-456-6789" />
 
-          <button type="submit">Send</button>
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" rows="5" required></textarea>
 
-        </form>
-        
-    </main>
+        <button type="submit">Send</button>
+    </form>
+
+</main>
+
   );
 }
