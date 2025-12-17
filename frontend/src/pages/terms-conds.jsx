33a09d668f44e113b04e34bb1de690d2f46d296e
Vincent Vue <vuevincent2025@gmail.com>
2025-12-17 09:19:12 -0500

    added base styling for terms & conditions page to match privacy policy page

---
+++ frontend/src/pages/terms-conds.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/terms-conds.css";
+
+export default function TermsConditions() {
+  return (
+    <main className="terms-conditions page">
+      <div className="terms-container">
+        
+        <header className="terms-title">
+          <h1>terms & conditions</h1>
+          <p className="terms-status">in effect: december 1, 2025</p>
+        </header>
+  
+      </div>
+    </main>
+  );
+}
