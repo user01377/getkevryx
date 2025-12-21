de3af988eb0587361ca8f74cbd8b7bdc55e617e5
Vincent Vue <vuevincent2025@gmail.com>
2025-12-21 17:09:56 -0600

    added data fields for all clothing products

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
-class Product:
-    __slots__ = []
-    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    colors = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.__name + "\n" + self.__description
