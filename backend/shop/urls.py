cca221b9cd84e2ec165713fb437c68b3effc4c88
Vincent Vue <vuevincent2025@gmail.com>
2025-12-23 10:13:35 -0600

    defined api routes for frontend to reach

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'), #home page for "all products"
+    path('products/', views.product_list, name='product_list'), # home page route for "all products"
+    path('cart/', views.get_cart, name="get_cart"),
+    path('cart/add/', views.add_to_cart, name="add_to_cart"),
+    path('cart/item/<int:item_id>/', views.update_cart_item, name="update_cart_item")
 ]
