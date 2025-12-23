89ba4bbf1a322594f83f848ee00df99df445cfb4
Vincent Vue <vuevincent2025@gmail.com>
2025-12-23 10:05:57 -0600

    created helper function which checks for existing user session

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,6 +1,19 @@
 from django.http import JsonResponse
-from .models import Product
+from .models import *
 
 def product_list(request):
     products = Product.objects.all().values()
     return JsonResponse(list(products), safe=False)
+
+def get_or_create_cart(request):
+    # helper which authenticates session existence, and returns cart
+    if not request.session.session_key:
+        request.session.create()
+    
+    session_key = request.session.session_key
+
+    cart, created = CreatedShoppingCart.objects.get_or_create(
+        session_key=session_key
+    )
+
+    return cart
