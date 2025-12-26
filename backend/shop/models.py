f441e66b2e29b5c19ddc4c171dbc60bd2c1ecc98
Vincent Vue <vuevincent2025@gmail.com>
2025-12-25 21:51:48 -0600

    in django models, added auto-gen slugs for each product
    will help with user experience and url routing

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
     description = models.TextField(blank=True)
     colors = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -10,9 +12,23 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     image_url = models.CharField(max_length=255)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         db_table = "products"
 
