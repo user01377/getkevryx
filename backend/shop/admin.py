db260f238c23cee8887624153f76f6a61f1d74b8
Vincent Vue <vuevincent2025@gmail.com>
2026-03-25 22:56:22 -0400

    removed all backend frameworks/files in prep for rewrite

--- backend/shop/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-class DataAdmin(admin.ModelAdmin):
-    list_display = ("name", "description","price", "stock", "category")
-    search_fields = ("name," "category")
