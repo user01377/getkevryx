2b0d967d6ba56027385e4020a554a1631cd4fbbc
Vincent Vue <vuevincent2025@gmail.com>
2026-05-23 01:21:50 -0500

    Added route to check connection to db

--- backend/routes.py
+++ backend/routes.py
@@ -1,1 +1,28 @@
 # the file that contains the router for the urls when api is called
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from database import get_db
+
+router = APIRouter()
+
+@router.get("/db-health")
+def db_health(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+
+        return {
+            "status": "healthy",
+            "database": "connected"
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "status": "unhealthy",
+                "database": "disconnected",
+                "error": str(e)
+            }
+        )
