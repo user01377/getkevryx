2179e8967ffd529e017d7c7f3cdbfb9b55ff9d35
user01377 <vuevincent2025@gmail.com>
2026-05-23 22:33:33 -0500

    Added table creation logic on app startup

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+import time
+from contextlib import asynccontextmanager
 from routes import router
+from database import Base, engine
+import models
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+    # add shutdown logic here
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
