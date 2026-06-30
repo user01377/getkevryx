a16a09afc5dfba4a590f4531a6a3bc777f199eb6
Vincent Vue <vuevincent2025@gmail.com>
2026-06-29 22:35:29 -0500

    Added test file for products route, and added basic test case

---
+++ backend/tests/test_products.py
@@ -0,0 +1,14 @@
+"""
+Tests the GET /products endpoint.
+"""
+
+def test_get_products_returns_all_products(client):
+    response = client.get("/products")
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert body["count"] == 2
+    assert len(body["data"]) == 2
+    assert body["data"][0]["name"] == "Test Shirt"
