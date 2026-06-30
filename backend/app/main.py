e741dd3cf9024f5ed2c816e4c367417f3fa5fe8c
Vincent Vue <vuevincent2025@gmail.com>
2026-06-29 21:50:47 -0500

    Move all main.py tasks into a function, allows for testing to be easier

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,24 +5,31 @@ from contextlib import asynccontextmanager
 from app.routes import router
 from app.startup import startup_backend, shutdown_scheduler
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    startup_backend()
+def create_app(enable_lifespan: bool = True):
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        if enable_lifespan:
+            startup_backend()
 
-    yield
+        yield
 
-    shutdown_scheduler()
+        if enable_lifespan:
+            shutdown_scheduler()
+        
+    app = FastAPI(lifespan=lifespan)
 
-app = FastAPI(lifespan=lifespan)
+    origins = os.getenv("CORS_ORIGINS").split(",")
 
-origins = os.getenv("CORS_ORIGINS").split(",")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    app.include_router(router)
 
-app.include_router(router)
+    return app
+
+app = create_app()
