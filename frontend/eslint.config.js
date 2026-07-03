d3183214111609c4a91fb4ab5714fa648bd43d30
Vincent Vue <vuevincent2025@gmail.com>
2026-07-03 08:54:24 -0500

    Configured eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+    ignores: [
+      "dist/**",
+      ".vite/**",
+      "coverage/**",
+      "node_modules/**",
     ],
+  },
+  
+  js.configs.recommended,
+
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,jsx,mjs,cjs}"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: "detect",
       },
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+  },
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
     },
   },
-])
+]);
