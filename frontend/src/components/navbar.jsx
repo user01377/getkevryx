f46769c36703aa764bfa08e27e031c5a3e1f845c
Vincent Vue <vuevincent2025@gmail.com>
2026-07-03 08:59:40 -0500

    Removed unused react imports

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/navbar.css";
 import { Link, useLocation } from "react-router-dom";
 
