375a0777687684d0f9423411ea1aa94b77c08083
Vincent Vue <vuevincent2025@gmail.com>
2026-07-19 14:44:41 -0500

    Implemented connection to redis with retry logic inside lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
-import os
+import os, logging, asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.routes import router
+from redis.asyncio import ConnectionPool, Redis
+from pyrate_limiter import RedisBucket, Rate, Duration, Limiter
 from app.startup import startup_backend
 
 
@@ -12,8 +14,31 @@ def create_app(enable_lifespan: bool = True):
         if enable_lifespan:
             startup_backend()
 
+            rates = [Rate(20, Duration.MINUTE)]
+            redis = Redis(connection_pool=ConnectionPool.from_url(f"redis://:{os.getenv("REDIS_PASSWORD")}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}"))
+
+            for i in range(1,6):
+                try:
+                    logging.info("Connecting to redis..")
+                    await redis.ping()
+                    logging.info("Redis connected.")
+                    break
+                except Exception as e:
+                    logging.error("Redis connection error, retrying in %s seconds", i ** 2)
+                    await asyncio.sleep(i ** 2)
+            else:
+                raise RuntimeError("Redis connection failed.")
+
+            bucket = await RedisBucket.init(rates, redis, "kevryx-rate-limit")
+
+            app.state.redis = redis
+            app.state.limiter = Limiter(bucket)
+
         yield
 
+        if enable_lifespan:
+            await app.state.redis.aclose()
+            
     app = FastAPI(lifespan=lifespan)
 
     origins = os.getenv("CORS_ORIGINS", "")
